55ffc7716e9c18fc72cd9bf131602a3877115830
KyleKatarn <kylekatarnls@gmail.com>
2017-03-09 16:24:07 +0100

    Use lcov format and lcov-result-merger for TravisCI

--- karma.conf.js
+++ karma.conf.js
@@ -28,9 +28,9 @@ module.exports = config => {
                     subdir: 'report'
                 },
                 {
-                    type : 'json',
+                    type : 'lcovonly',
                     subdir: './',
-                    file: 'front.json'
+                    file: 'coverage-front.info'
                 }
             ]
         }
