781ae79ee6dcae97b1650bef2902fa2c9e6a897a
KyleKatarn <kylekatarnls@gmail.com>
2017-03-13 22:33:29 +0100

    Simplify logs in the example app

--- example/index.js
+++ example/index.js
@@ -6,10 +6,8 @@ app.listen(8091);
 app.use(express.static(__dirname + '/static'));
 app.use(express.static(__dirname + '/../lib'));
 
-Momentum.connect(app, 'mongodb://localhost:27017/momentum').then(momentum => {
-    // momentum.setAuthorizationStrategy((method, args, request, response) => {
-    //     console.log('foo')
-    // });
+Momentum.connect(app, 'mongodb://localhost:27017/momentum').then(() => {
+    opn('http://localhost:8091');
 });
 
-opn('http://localhost:8091');
+
